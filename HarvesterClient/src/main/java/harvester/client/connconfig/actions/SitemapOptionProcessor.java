4a6f1780edfbb35e6e1ea22b5022bcc0cf7e41d9
tingram <tingram@775b9859-8e63-0410-9d1e-8bd1c4a20c51>
2010-09-10 06:00:42 +0000

    Update the client interface to cater for Sitemaps as harvest option.
    git-svn-id: http://svn.nla.gov.au/svn/public/nlaharvester/trunk@121 775b9859-8e63-0410-9d1e-8bd1c4a20c51

---
+++ HarvesterClient/src/main/java/harvester/client/connconfig/actions/SitemapOptionProcessor.java
@@ -0,0 +1,29 @@
+package harvester.client.connconfig.actions;
+
+import harvester.client.connconfig.StepParameterView;
+import harvester.client.data.dao.DAOFactory;
+import harvester.data.Contributor;
+
+import java.util.List;
+import java.util.Map;
+
+public class SitemapOptionProcessor implements StepOptionProcessor {
+
+	public int getHtype() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void postProcess(Map<Integer, String> parameters,
+			DAOFactory daofactory, Contributor c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean process(List<StepParameterView> spv, DAOFactory daofactory,
+			Contributor c) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
