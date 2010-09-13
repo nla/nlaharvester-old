d316a3b0e5afe4f26713abb92c7c911a9e11ee4f
tingram <tingram@775b9859-8e63-0410-9d1e-8bd1c4a20c51>
2010-09-13 07:19:10 +0000

    Remove unwanted imports
    git-svn-id: http://svn.nla.gov.au/svn/public/nlaharvester/trunk@127 775b9859-8e63-0410-9d1e-8bd1c4a20c51

--- HarvesterProcessor/src/test/java/contributors/ABCRadioNationalTests.java
+++ HarvesterProcessor/src/test/java/contributors/ABCRadioNationalTests.java
@@ -7,15 +7,12 @@ import harvester.processor.test.helpers.MockStepLogger;
 import harvester.processor.util.HTMLHelper;
 
 import java.io.IOException;
-import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.util.HashMap;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
-import org.dom4j.io.OutputFormat;
-import org.dom4j.io.XMLWriter;
 import org.junit.Ignore;
 import org.junit.Test;
 
