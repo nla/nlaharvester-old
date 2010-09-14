c17655fe772f05b14506db6d83edf4c23ca4b6ad
tingram <tingram@775b9859-8e63-0410-9d1e-8bd1c4a20c51>
2010-09-14 02:07:31 +0000

    Removed unwanted tests
    git-svn-id: http://svn.nla.gov.au/svn/public/nlaharvester/trunk@131 775b9859-8e63-0410-9d1e-8bd1c4a20c51

--- HarvesterProcessor/src/test/java/harvester/processor/util/HTMLHelperTest.java
+++ HarvesterProcessor/src/test/java/harvester/processor/util/HTMLHelperTest.java
@@ -2,11 +2,6 @@ package harvester.processor.util;
 
 import static org.junit.Assert.*;
 
-import java.io.UnsupportedEncodingException;
-
-import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class HTMLHelperTest {
@@ -19,28 +14,4 @@ public class HTMLHelperTest {
 		assertTrue(page.contains("<html"));
 	}
 		
-	@Test
-	public void shouldRemoveUnwantedCharactersBeforeHtml() {
-		String actual = HTMLHelper.stripAnythingBeforeOpeningHtmlTag("abcd<html>");
-		assertEquals("<html>", actual);
-	}
-	
-	@Test
-	public void shouldRemoveNbsp() {
-		String actual = HTMLHelper.replaceNbspCharactersWithSpace("<html>&nbsp;yay&nbsp;</html>");
-		assertEquals("<html> yay </html>", actual);
-	}
-	
-	@Test
-	public void shouldCleanUpBadHtml() throws UnsupportedEncodingException {		
-		String badHtml = "<html><div></html>";
-		String goodHtml = HTMLHelper.cleanHtmlToXml(badHtml);
-		goodHtml = HTMLHelper.stripAnythingBeforeOpeningHtmlTag(goodHtml);
-		
-		try {
-			DocumentHelper.parseText(goodHtml);
-		} catch (DocumentException e) {
-			Assert.fail("Unable to clean the html document. \n" + e);
-		}
-	}	
 }
